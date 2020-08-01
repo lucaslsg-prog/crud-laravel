1712250620716941de2db0bb34d40158510cf0ee
lucaslsg-prog <lsg.comunicacoes@gmail.com>
2020-08-01 16:08:34 -0400

    Navegação Pessoas

---
+++ resources/views/pessoa/create.blade.php
@@ -0,0 +1,12 @@
+<h1>Formulário de Pessoa</h1>
+
+<form action="/pessoa" method="post">
+    <input type="text" name="nome">
+    <input type="text" name="telefone">
+    <input type="text" name="email">
+    <button type="submit">Salvar</button>
+</form>
+
+<br>
+
+<a href="/pessoa">Voltar</a>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pessoas', 'PessoaController@index');
+
+Route::get('pessoas/create', 'PessoaController@create');
